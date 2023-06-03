48896eeeaadea4393398d67e02cead53ad400196
Glitch (anlisa-portfolio-clever1) <none>
2023-06-03 05:46:01 +0000

    😏💔 Updated with Glitch

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
 const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
@@ -20,24 +9,22 @@ if (btn) {
     btn.classList.toggle("dipped");
   };
 }
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+console.log('stuff')
 
 $('.Ele').click(function(){
   $('.imger').attr("src","https://s3-us-west-2.amazonaws.com/schoolmint-chooser-media/oakland/images/606-2.jpg");
   $('.cptner').text("My Elementary School");
+  console.log('stuff1')
 });
 
 $('.Middle').click(function(){
   $('.imger').attr("src","https://s3-us-west-2.amazonaws.com/schoolmint-chooser-media/oakland/images/642.jpg");
   $('.cptner').text("My Middle School");
+  console.log('stuff2')
 });
 
 $('.High').click(function(){
   $('.imger').attr("src","https://upload.wikimedia.org/wikipedia/commons/b/b9/Oakland_Technical_High.jpg");
   $('.cptner').text("My High School");
+  console.log('stuff3')
 }); 
